ee3dfefd45b743b4db2db452bbbe962577008786
fwio <624835339@qq.com>
2023-04-05 23:17:17 +0800

    fix(playwright): increase timeout for `expect`

--- printer/playwright.config.ts
+++ printer/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 50 * 1000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
