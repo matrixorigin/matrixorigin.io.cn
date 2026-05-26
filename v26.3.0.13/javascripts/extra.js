a763be308068479e437cec4fece7f29ea0b9731c
sukki37 <cat0043@outlook.com>
2026-05-26 07:51:57 +0000

    Deployed dbd59a4a1a to v26.3.0.13 with MkDocs 1.6.1 and mike 1.1.2

---
+++ v26.3.0.13/javascripts/extra.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?5b7b3f6f6d3c71ccfb2011dba89bac2f';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
